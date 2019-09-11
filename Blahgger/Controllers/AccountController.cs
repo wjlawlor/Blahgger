ab6bb3e9f320fe61a0ed7cf037ac8b51b4919e70
wjlawlor <wjl28@msn.com>
2019-09-11 11:01:55 -0400

    Working registration.

--- Blahgger/Controllers/AccountController.cs
+++ Blahgger/Controllers/AccountController.cs
@@ -1,8 +1,6 @@
 ﻿using Blahgger.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Web.Mvc;
 using System.Web.Security;
 
@@ -13,20 +11,90 @@ namespace Blahgger.Controllers
         [AllowAnonymous]
         public ActionResult Login()
         {
+            // string hashedPassword = BCrypt.Net.BCrypt.HashPassword("password", 12);
+
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
             return View();
         }
 
         [HttpPost]
-        public ActionResult Login(User user)
+        public ActionResult Login(LoginUser user)
         {
+            // Did the user provide values?
             if (ModelState.IsValidField("Email") && ModelState.IsValidField("Password"))
             {
                 // TODO get the user record from DB.
+                string connectionString = ConfigurationManager.ConnectionStrings["BlahggerDatabase"].ConnectionString;
 
-                // Check if the password matches.
-                if (!(user.Email == "james@smashdev.com" && user.Password == "password"))
+                using (SqlConnection connection = new SqlConnection(connectionString))
                 {
-                    ModelState.AddModelError("", "Login failed.");
+                    connection.Open();
+                    string sql = @"
+                            SELECT HashedPassword
+                            FROM [User]
+                            WHERE Email = @Email
+                        ";
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    command.Parameters.AddWithValue("@Email", user.Email);
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    string hashedPassword = null;
+
+                    while (reader.Read())
+                    {
+                        hashedPassword = reader.GetString(0);
+                    }
+
+                    // Check if the password matches.
+                    if (string.IsNullOrEmpty(hashedPassword) || 
+                        !BCrypt.Net.BCrypt.Verify(user.Password, hashedPassword))
+                    {
+                        // Failed to find a record with the provided email.
+                        ModelState.AddModelError("", "Login failed.");
+                    }
+                }
+            }
+
+            if (ModelState.IsValid)
+            {
+                // Login the User.
+                FormsAuthentication.SetAuthCookie(user.Email, false);
+
+                // Send to homepage.
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterUser user)
+        {
+            if (ModelState.IsValidField("Email") && ModelState.IsValidField("FirstName")
+                && ModelState.IsValidField("LastName") && ModelState.IsValidField("Password"))
+            {
+                string hashedPassword = BCrypt.Net.BCrypt.HashPassword(user.Password, 12);
+
+                string connectionString = ConfigurationManager.ConnectionStrings["BlahggerDatabase"].ConnectionString;
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sql = @"
+                            INSERT INTO [User] (Email, FirstName, LastName, HashedPassword)
+                            VALUES (@Email, @FirstName, @LastName, @HashedPassword)
+                        ";
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    command.Parameters.AddWithValue("@Email", user.Email);
+                    command.Parameters.AddWithValue("@FirstName", user.FirstName);
+                    command.Parameters.AddWithValue("@LastName", user.LastName);
+                    command.Parameters.AddWithValue("@HashedPassword", hashedPassword);
+                    command.ExecuteNonQuery();
                 }
             }
 
---
+++ Blahgger/Models/RegisterUser.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blahgger.Models
+{
+    public class RegisterUser
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName{ get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+        public string HashedPassword { get; set; }
+    }
+}
