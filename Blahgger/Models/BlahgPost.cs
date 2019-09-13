c97fdbdda18eef46b526e452ec37756acdee1b75
wjlawlor <wjl28@msn.com>
2019-09-13 17:05:53 -0400

    Authentication working

---
+++ Blahgger/Models/BlahgPost.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blahgger.Models
+{
+    public class BlahgPost
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+    }
+}
---
+++ Blahgger/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blahgger.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int BlahgPostId { get; set; }
+        public int UserId { get; set; }
+        public string Text { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+    }
+}
